ed7493483a73afad22d0018c28cdb3e258d3a978
KeldeenStevens <keldeenclarkperalta@gmail.com>
2026-04-30 18:37:04 -0400

    accesoalsistema y validacion del PIN

--- gittest/sistemaderetiroautomatico/actualcode.c
+++ gittest/sistemaderetiroautomatico/actualcode.c
@@ -10,3 +10,26 @@ int intentos = 0;
 void menu();
 void retirar();
 void depositar();
+
+int main() {
+    int pin;
+
+    printf("=== BIENVENIDO AL ATM ===\n");
+
+    while (intentos < 3) {
+        printf("Ingrese su PIN: ");
+        scanf("%d", &pin);
+
+        if (pin == pinCorrecto) {
+            printf("Acceso concedido\n");
+            menu();
+            return 0;
+        } else {
+            printf("PIN incorrecto\n");
+            intentos++;
+        }
+    }
+
+    printf("Cuenta bloqueada\n");
+    return 0;
+}
