cde260fc61d54022d53d218663b835ef401b0efc
KeldeenStevens <keldeenclarkperalta@gmail.com>
2026-04-30 11:27:47 -0400

    Introducción a las variables

---
+++ gittest/sistemaderetiroautomatico/actualcode.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Variables globales
+int saldo = 1000;
+int pinCorrecto = 1234;
+int intentos = 0;
+
+// Prototipos
+void menu();
+void retirar();
+void depositar();
